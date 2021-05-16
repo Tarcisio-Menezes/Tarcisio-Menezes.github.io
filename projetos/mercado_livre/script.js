b158d7aab90c17cf8012c4f2dce3eb1fccfdc826
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-05-16 15:02:28 -0300

    estilizaçoes project shopping cart

--- projetos/mercado_livre/script.js
+++ projetos/mercado_livre/script.js
@@ -35,7 +35,7 @@ function cartItemClickListener(event) {
 function createCartItemElement({ id: ItemID, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${ItemID} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.innerHTML = `SKU: ${ItemID} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
  }
